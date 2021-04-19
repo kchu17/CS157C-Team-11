71730f3f414119202bea5e6c1545fc0f65d08df5
SanjanaKonduru1 <47699961+SanjanaKonduru1@users.noreply.github.com>
2021-04-19 16:58:26 -0700

    Added 10 sample entries

--- MIAPython.py
+++ MIAPython.py
@@ -2,9 +2,9 @@ from cloudant.client import Cloudant
 from cloudant.error import CloudantException
 from cloudant.result import Result, ResultByKey
 
-client = Cloudant.iam("685542ea-039d-4fbb-a0f3-58372ca5e9da-bluemix", "J_VIN6hP17Zu5r4rUUGesjutWnRPrbie9BT1Z4O-Unvj", connect=True)
+client = Cloudant.iam("dcc9e7a6-300c-48bc-a169-3b153750d14a-bluemix", "ok15p-S7RlG8WsXUDBKLVFyWPtzneRXP8kshX1eRjWUx", connect=True)
 
-databaseName = "databasedemo"
+databaseName = "databaseMIA"
 myDatabaseDemo = client.create_database(databaseName)
 if myDatabaseDemo.exists():
  print("'{0}' successfully created.\n".format(databaseName))
@@ -19,8 +19,16 @@ def addNewEntry(question,answer):
     if my_document.exists():
         print('SUCCESS!!')
 
-addNewEntry("How old is Roshan?","3")
-addNewEntry("How ugly is Roshan?","10")
+addNewEntry("How old is Joe Biden?","78")
+addNewEntry("Whats the mascot for SJSU?","Sammy the Spartan")
+addNewEntry("Who is the CEO of Apple?","Tim Cook")
+addNewEntry("How many cups are in a gallon?","16")
+addNewEntry("What is the unit conversion from pounds to kilograms?","One pound is 2.2 kilograms")
+addNewEntry("How much sugar is in a can of coke?","39 grams of sugar")
+addNewEntry("Is the United States more populated than Russia?","Yes, the United States has approximately 330 million people while Russia has approximately 145 million.")
+addNewEntry("Whats Standford University's Acceptance Rate?","4.3%")
+addNewEntry("Is Pluto still a planet?","No, it was deemed a dwarf planet in 2006")
+addNewEntry("What NBA team has the most championships?","The LA Lakers and the Boston Celtics are currently tied for the most championships with 17 each. ")
 
 #READ
 def readEntry(question):
@@ -31,5 +39,13 @@ def readEntry(question):
 
     print("Answer not found in database")
 
-readEntry("How old is Roshan?")
-readEntry("How smart is Roshan?")
+readEntry("How old is Joe Biden?")
+readEntry("Whats the mascot for SJSU?")
+readEntry("Who is the CEO of Apple?")
+readEntry("How many cups are in a gallon?")
+readEntry("What is the unit conversion from pounds to kilograms?")
+readEntry("How much sugar is in a can of coke?")
+readEntry("Is the United States more populated than Russia?")
+readEntry("Whats Standford University's Acceptance Rate?")
+readEntry("Is Pluto still a planet?")
+readEntry("What NBA team has the most championships?")
